30d6fdb010a9e2888d684c920c55236318af26a3
tianyu5177 <zhangtianyu5177@sina.com>
2020-01-03 17:01:10 +0800

    init

--- 1.原生ajax/server.js
+++ 1.原生ajax/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 //使用中间件完成解析post请求请求体中参数的解析
 app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.use(express.static(__dirname+'/public'))
 
 //3.配置路由
@@ -15,13 +16,16 @@ app.get('/test_get',(request,response)=>{
 //3.配置路由
 app.post('/test_post',(request,response)=>{
   console.log('请求携带的参数是：',request.body)
+  console.log('请求携带的参数是：',request.query)
   response.send('你发过来的是post请求，我是服务器返回的一些数据')
 })
 
 //4.绑定端口监听
 app.listen(3000,(err)=>{
   if(!err) {
+    console.log('小兄弟，不要通过编译器打开html页面，会有跨域问题！！！用下面的地址：')
     console.log('练习原生js发送ajax_get请求的页面地址为：http://localhost:3000/ajax_get.html')
+    console.log('练习原生js发送ajax_post请求的页面地址为：http://localhost:3000/ajax_post.html')
   }
   else console.log(err)
 })
